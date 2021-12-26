01034cab627cd7f8e1734291b4c5c2f2b201983a
Bulat Maskurov <mbmhd2015@gmail.com>
2021-12-26 18:19:15 +0300

    Убрал сериализацию пользователя в json

--- src/test/java/ru/bulkashmak/ApiTest.java
+++ src/test/java/ru/bulkashmak/ApiTest.java
@@ -13,16 +13,6 @@ import java.util.Random;
 
 public class ApiTest {
 
-    User user1 = new User(
-            generateRandomString()+"qwer@mail.com",
-            "Rest"+generateRandomString(),
-            new int[]{12},
-            new int[]{36,37},
-            "Archery",
-            "333 33 33",
-            "123456789012"
-    );
-
     static String generateRandomString() {
         int leftLimit = 97;
         int rightLimit = 122;
@@ -38,30 +28,37 @@ public class ApiTest {
     @DataProvider(name = "jsonDataProvider")
     private Object[][] userCreation() throws JsonProcessingException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String userData = objectMapper.writeValueAsString(user1);
         return new Object[][] {
-            new Object[] {userData}
+            new Object[] {
+                new User(
+                    generateRandomString()+"qwer@mail.com",
+                    "Rest"+generateRandomString(),
+                    new int[]{12},
+                    new int[]{36,37},
+                    "Archery",
+                    "333 33 33",
+                    "123456789012"
+                )
+            }
         };
-
     }
 
     @Test(dataProvider = "jsonDataProvider", description = "Позитивный сценарий создания пользователя")
-    void simpleTest(String jsonUserData) {
+    void simpleTest(User userData) {
 
         RestAssured.given()
                 .baseUri("http://users.bugred.ru/tasks")
                 .basePath("/rest/createuser")
                 .contentType("application/json")
-                .body(jsonUserData)
+                .body(userData)
                 .when()
                     .post()
                 .then()
                     .contentType(ContentType.JSON)
                     .assertThat().statusCode(200)
-                    .body("name", Matchers.equalTo(user1.getName()))
-                    .body("email", Matchers.equalTo(user1.getEmail()))
-                    .body("hobby", Matchers.equalTo(user1.getHobby()))
-                    .body("phone", Matchers.equalTo(user1.getPhone()));
+                    .body("name", Matchers.equalTo(userData.getName()))
+                    .body("email", Matchers.equalTo(userData.getEmail()))
+                    .body("hobby", Matchers.equalTo(userData.getHobby()))
+                    .body("phone", Matchers.equalTo(userData.getPhone()));
     }
 }
