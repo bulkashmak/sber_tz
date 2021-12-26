0b33bcaa4b88838ed66118a946d1ff47eef0eb54
Bulat Maskurov <mbmhd2015@gmail.com>
2021-12-26 21:38:09 +0300

    Имплементация Шагов

--- src/test/java/ru/bulkashmak/ApiTest.java
+++ src/test/java/ru/bulkashmak/ApiTest.java
@@ -1,17 +1,19 @@
 package ru.bulkashmak;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Random;
 
-@Test
+@Test(description = "Позитивный сценарий создания пользователя")
 public class ApiTest extends AllureRestAssured {
 
     static String generateRandomString() {
@@ -27,41 +29,67 @@ public class ApiTest extends AllureRestAssured {
     }
 
     @DataProvider(name = "jsonDataProvider")
-    private Object[][] userCreation() throws JsonProcessingException {
+    private Object[][] userCreation() {
 
-        return new Object[][] {
-            new Object[] {
-                new User(
-                    generateRandomString()+"qwer@mail.com",
-                    "Rest"+generateRandomString(),
-                    new int[]{12},
-                    new int[]{36,37},
-                    "Archery",
-                    "333 33 33",
-                    "123456789012"
-                )
-            }
+        return new Object[][]{
+                new Object[]{
+                        new User(
+                                generateRandomString() + "qwer@mail.com",
+                                "Rest" + generateRandomString(),
+                                new int[]{12},
+                                new int[]{36, 37},
+                                "Archery",
+                                "333 33 33",
+                                "123456789012"
+                        )
+                }
         };
     }
 
-    @Step("User creation")
-    @Test(dataProvider = "jsonDataProvider", description = "Позитивный сценарий создания пользователя")
-    void simpleTest(User userData) {
+    @Step("Отправка запроса")
+//    @Test(dataProvider = "jsonDataProvider")
+    static Response userCreationRequest(User userData) {
 
-        RestAssured.given()
+        Response response = RestAssured.given()
                 .baseUri("http://users.bugred.ru/tasks")
                 .basePath("/rest/createuser")
                 .contentType("application/json")
                 .body(userData)
                 .when()
-                    .filter(new AllureRestAssured())
-                    .post()
+                .filter(new AllureRestAssured())
+                .post()
                 .then()
-                    .contentType(ContentType.JSON)
-                    .assertThat().statusCode(200)
-                    .body("name", Matchers.equalTo(userData.getName()))
-                    .body("email", Matchers.equalTo(userData.getEmail()))
-                    .body("hobby", Matchers.equalTo(userData.getHobby()))
-                    .body("phone", Matchers.equalTo(userData.getPhone()));
+                .contentType(ContentType.JSON)
+                .assertThat().statusCode(200)
+//                .body("name", Matchers.equalTo(userData.getName()))
+//                .body("email", Matchers.equalTo(userData.getEmail()))
+//                .body("hobby", Matchers.equalTo(userData.getHobby()))
+//                .body("phone", Matchers.equalTo(userData.getPhone()))
+                .extract().response();
+
+        return response;
+    }
+
+    @Step
+    @Test(dataProvider = "jsonDataProvider")
+    void validateResponse(User userData) {
+
+        Response response = userCreationRequest(userData);
+
+        String stringResponse = response.body().toString();
+
+        JSONObject jsonObject = new JSONObject(stringResponse);
+
+        Assert.assertEquals(jsonObject.getString("name"), userData.getName());
+        Assert.assertEquals(jsonObject.getString("email"), userData.getEmail());
+        Assert.assertEquals(jsonObject.getString("hobby"), userData.getHobby());
+        Assert.assertEquals(jsonObject.getString("phone"), userData.getPhone());
+
+
+//        .body("name", Matchers.equalTo(userData.getName()))
+//                .body("email", Matchers.equalTo(userData.getEmail()))
+//                .body("hobby", Matchers.equalTo(userData.getHobby()))
+//                .body("phone", Matchers.equalTo(userData.getPhone()))
+
     }
 }
